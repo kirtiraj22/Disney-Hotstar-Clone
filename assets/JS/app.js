20ddeeeba2cf5a188270971d8c4411b6132ede85
Kirtiraj <83873093+kirtiraj22@users.noreply.github.com>
2022-11-15 21:23:14 +0530

    Update app.js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -88,23 +88,3 @@ cardContainers.forEach((item , i) => {
         item.scrollLeft -= containerWidth + 200;
     })
 })
-
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar"); 
-let cancelBtn = document.querySelector(".cancel-btn");
-let menuBtn = document.querySelector(".menu-btn");
-
-menuBtn.onclick = function(){
-    navBar.classList.add("active");
-    menuBtn.style.opacity = "0";
-    menuBtn.style.pointerEvents = "none";
-    body.style.overflowX = "hidden";
-    scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-    body.style.overflow = "auto";
-    scrollBtn.style.pointerEvents = "auto";
-}
