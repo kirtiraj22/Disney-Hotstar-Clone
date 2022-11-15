f1ba248a53ef622664d543cd2e6df7a870824f71
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2022-11-15 21:13:12 +0530

    initial commit

---
+++ assets/JS/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: "falcon and the winter soldier",
+        des: "Six months after being restored to life in the Blip and being handed Captain America's shield by Steve Rogers, Sam Wilson is working with the U.S. Air Force overseas to rescue a hostage from LAF, a terrorist group led by Georges Batroc.",
+        image: 'assets/images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: "Loki revolves around the mischievous villain escaping the clutches of The Avengers and getting caught by the Time Variance Authority.",
+        image: 'assets/images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: "It follows Wanda Maximoff and Vision as they live an idyllic suburban life in the town of Westview, New Jersey, until their reality starts moving through different decades of sitcom homages and television tropes.",
+        image: 'assets/images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: "Raya and the Last Dragon blends classic Disney elements with the company's contemporary focus on diverse stories and characters.",
+        image: 'assets/images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: "Set on the Italian Riviera, the film centers on Luca Paguro, a young sea monster boy with the ability to assume human form while on land, who explores the town of Portorosso with his new best friends, Alberto Scorfano and Giulia Marcovaldo, experiencing a life-changing summer adventure.",
+        image: 'assets/images/slider 5.PNG'
+    }
+]
